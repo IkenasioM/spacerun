d74401ef1449a6d3f77366b284c8642eb96bd865
Unknown <ikenasio.m@hotmail.fr>
2018-02-26 17:03:10 +0100

    Ajout fond gameplay

---
+++ Assets/Script/InstantiateObjectCanvas.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateObjectCanvas : MonoBehaviour {
+    [SerializeField] GameObject Vehicule;
+    void Start () {
+        GameObject enemy = Instantiate(Vehicule, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+        enemy.transform.SetParent(GameObject.FindGameObjectWithTag("Canvas").transform, false);
+    }
+}
