89abb9792d7ba29da092a4098b417d6ec24389aa
Alexandre <alexandre.segretin@etudiant.univ-lr.fr>
2018-03-05 15:32:32 +0100

    rectification page d'option

---
+++ Assets/Script/MainScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainScript : MonoBehaviour {
+
+    public string sceneAccueil;
+    public string sceneJeux;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void chargementScene(int a){
+        if (a == 0)
+        {
+            SceneManager.LoadScene(this.sceneAccueil);
+            return;
+        }
+        if (a == 1)
+        {
+            SceneManager.LoadScene(this.sceneJeux);
+            return;
+        }
+        Debug.Log("indexer non trouvé");
+    }
+
+    public void exitGame() {
+        Application.Quit();
+    }
+}
